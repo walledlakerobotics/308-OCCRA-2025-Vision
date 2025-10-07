637694fad86bc71fe5a0ef7d773e0e34f71b716e
tg1523342 <tg1523342@icloud.com>
2025-10-07 17:19:31 -0400

    Created Camera class to allow more than one place to read from any given camera

---
+++ src/cube_detector/camera.py
@@ -0,0 +1,61 @@
+import cv2
+import numpy as np
+from queue import Empty, Queue
+from threading import Thread, Lock
+
+
+def read_camera(index: int):
+    camera = cv2.VideoCapture(index)
+
+    while True:
+        ret, frame = camera.read()
+        if not ret:
+            continue
+
+        with Camera.queue_locks[index]:
+            if index not in Camera.queues:
+                del Camera.queue_locks[index]
+                break
+
+            for i, q in enumerate(Camera.queues[index]):
+                if i == len(Camera.queues[index]) - 1:
+                    q.put(frame) # don't create extra copy
+                    continue
+
+                q.put(frame.copy())
+
+
+class Camera:
+    queues: dict[int, list[Queue]] = {}
+    queue_locks: dict[int, Lock] = {}
+
+    index: int
+    queue: Queue[cv2.typing.MatLike]
+
+    def __init__(self, index: int):
+        if not index in Camera.queue_locks:
+            Camera.queue_locks[index] = Lock()
+
+        with Camera.queue_locks[index]:
+            if index not in Camera.queues:
+                Camera.queues[index] = []
+                thread = Thread(target=read_camera, args=(index,))
+                thread.start()
+
+            self.index = index
+            self.queue = Queue(maxsize=1)
+            
+            Camera.queues[index].append(self.queue)
+
+    def __del__(self):
+        with Camera.queue_locks[self.index]:
+            Camera.queues[self.index].remove(self.queue)
+
+            if not Camera.queues[self.index]:
+                del Camera.queues[self.index]
+
+    def read(self) -> tuple[bool, cv2.typing.MatLike]:
+        try:
+            return True, self.queue.get_nowait()
+        except Empty:
+            return False, np.zeros((480, 640, 3), dtype=np.uint8)
