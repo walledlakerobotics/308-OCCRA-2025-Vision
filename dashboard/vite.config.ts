8e245100db95f1355cb9fab0ec94ff1a763a8c39
tg1523342 <tg1523342@icloud.com>
2025-10-06 21:54:15 -0400

    Moved dashboard code outside of backend src folder

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -5,7 +5,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig({
   plugins: [svelte()],
   build: {
-    outDir: '../cube_detector/static',
+    outDir: '../src/cube_detector/static',
     emptyOutDir: true
   }
 })
