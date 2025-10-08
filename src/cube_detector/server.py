c8f33d4ad3df2d351dd4e754fb5aa17d42d999c9
tg1523342 <tg1523342@icloud.com>
2025-10-08 17:32:21 -0400

    Added camera stream route

--- src/cube_detector/server.py
+++ src/cube_detector/server.py
@@ -1,6 +1,30 @@
-from flask import Flask
+import cv2
+from flask import Flask, Response
+from .camera import Camera
 
 app = Flask(__name__, static_url_path="/")
+
+
+def generate_camera_feed(index: int):
+    camera = Camera(index)
+    
+    while True:
+        ret, frame = camera.read()
+        if not ret:
+            continue
+
+        img = cv2.imencode(".jpg", frame)[1].tobytes()
+
+        yield (b"--frame\r\n"
+               b"Content-Type: image/jpeg\r\n\r\n" + img)
+
+
+@app.route("/stream/<int:index>")
+def stream(index: int):
+    return Response(
+        generate_camera_feed(index),
+        mimetype="multipart/x-mixed-replace; boundary=frame",
+    )
 
 
 @app.route("/index.html")
