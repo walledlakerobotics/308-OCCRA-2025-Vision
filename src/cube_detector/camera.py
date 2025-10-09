0b30267b015647367ff1c337b4153495671f03b3
tg1523342 <tg1523342@icloud.com>
2025-10-09 12:58:51 -0400

    Added support for extra streams

--- src/cube_detector/camera.py
+++ src/cube_detector/camera.py
@@ -1,7 +1,14 @@
 import cv2
 import numpy as np
+import time
+
+from flask import Response
+from typing import ClassVar
 from queue import Empty, Queue
 from threading import Thread, Lock
+
+type Frame = cv2.typing.MatLike
+type FrameQueue = Queue[Frame]
 
 
 def read_camera(index: int):
@@ -10,6 +17,7 @@ def read_camera(index: int):
     while True:
         ret, frame = camera.read()
         if not ret:
+            time.sleep(0.01)
             continue
 
         with Camera.queue_locks[index]:
@@ -19,33 +27,40 @@ def read_camera(index: int):
 
             for i, q in enumerate(Camera.queues[index]):
                 if i == 0:
-                    q.put(frame) # don't create extra copy
+                    q.put(frame)  # don't create extra copy
                     continue
 
                 q.put(frame.copy())
 
 
 class Camera:
-    queues: dict[int, list[Queue]] = {}
-    queue_locks: dict[int, Lock] = {}
+    queues: ClassVar[dict[int, list[FrameQueue]]] = {}
+    queue_locks: ClassVar[dict[int, Lock]] = {}
+
+    streams: ClassVar[dict[int, dict[str, FrameQueue]]] = {}
+    stream_locks: ClassVar[dict[int, Lock]] = {}
 
     index: int
-    queue: Queue[cv2.typing.MatLike]
+    queue: FrameQueue
 
     def __init__(self, index: int):
-        if not index in Camera.queue_locks:
-            Camera.queue_locks[index] = Lock()
+        self.index = index
+        self.queue = Queue(maxsize=1)
 
-        with Camera.queue_locks[index]:
-            if index not in Camera.queues:
-                Camera.queues[index] = []
-                thread = Thread(target=read_camera, args=(index,))
+        if not self.index in Camera.queue_locks:
+            Camera.queue_locks[self.index] = Lock()
+            Camera.stream_locks[self.index] = Lock()
+
+        with Camera.queue_locks[self.index]:
+            if self.index not in Camera.queues:
+                Camera.queues[self.index] = []
+                thread = Thread(target=read_camera, args=(self.index,))
                 thread.start()
 
-            self.index = index
-            self.queue = Queue(maxsize=1)
-            
-            Camera.queues[index].append(self.queue)
+            Camera.queues[self.index].append(self.queue)
+
+        with Camera.stream_locks[self.index]:
+            Camera.streams[self.index] = {}
 
     def __del__(self):
         with Camera.queue_locks[self.index]:
@@ -54,8 +69,54 @@ class Camera:
             if not Camera.queues[self.index]:
                 del Camera.queues[self.index]
 
-    def read(self) -> tuple[bool, cv2.typing.MatLike]:
+    def read(self) -> tuple[bool, Frame]:
         try:
             return True, self.queue.get_nowait()
         except Empty:
             return False, np.zeros((480, 640, 3), dtype=np.uint8)
+
+    def stream(self, name: str):
+        with Camera.stream_locks[self.index]:
+            if name not in Camera.streams[self.index]:
+                Camera.streams[self.index][name] = Queue(maxsize=1)
+
+            return self.Stream(Camera.streams[self.index][name])
+
+    @classmethod
+    def stream_response(cls, index: int, name: str):
+        camera = Camera(index)
+
+        if name == "raw":
+            stream = camera
+        else:
+            stream = camera.stream(name)
+
+        def gen_frames():
+            while True:
+                ret, frame = stream.read()
+                if not ret:
+                    continue
+
+                img = cv2.imencode(".jpg", frame)[1].tobytes()
+
+                yield (b"--frame\r\n"
+                       b"Content-Type: image/jpeg\r\n\r\n" + img)
+
+        return Response(
+            gen_frames(), mimetype="multipart/x-mixed-replace; boundary=frame"
+        )
+
+    class Stream:
+        queue: FrameQueue
+
+        def __init__(self, queue: FrameQueue):
+            self.queue = queue
+
+        def send(self, frame: Frame):
+            self.queue.put(frame)
+
+        def read(self) -> tuple[bool, Frame]:
+            try:
+                return True, self.queue.get_nowait()
+            except Empty:
+                return False, np.zeros((480, 640, 3), dtype=np.uint8)
