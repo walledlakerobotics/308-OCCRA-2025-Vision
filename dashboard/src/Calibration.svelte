313f3996767b4772ad2da78d8b5631d80cf57ca1
whiteswordv <143036168+whiteswordv@users.noreply.github.com>
2025-11-18 20:59:56 -0500

    Update Calibration.svelte

--- dashboard/src/Calibration.svelte
+++ dashboard/src/Calibration.svelte
@@ -1,75 +1,61 @@
-<script lang="ts">
-    import placeholder from "./img/placeHolder.jpg";
-</script>
+    <script lang="ts">
+        import placeholder from "./img/placeHolder.jpg";
+    </script>
 
-<div class="cameraContainer">
-    <img src="{placeholder}" alt="">
-</div>
+    <div class="cameraContainer">
+        <div class="camImage">
+            <img src="{placeholder}" alt="">
+        </div>
+    </div>
 
-<div class="propertieSettings">
-    <select name="CameraList">
-        <option value="camera 121"> ;3</option>
-    </select>
-</div>
+    <div class="propertieSettings">
+        <select name="CameraList">
+            <option value="camera 121"> ;3</option>
+        </select>
+    </div>
 
-<style>
-    .propertieSettings {
-        background-color: var(--foregroundColor);
+    <style>
+        .propertieSettings {
+            background-color: var(--foregroundColor);
 
 
-        height: 100%;
-        padding: 10px;
+            height: 100%;
+            padding: 10px;
 
-        margin: 0%;
-        display: flex;
-    }
+            margin: 0%;
+            display: flex;
+        }
 
-    .cameraContainer  {
-        background-color: var(--foregroundColor);
+        select {
+            color: var(--borderColor);
+            background-color: rgb(11, 1, 28);
+            margin-left: auto; margin-right: auto;
 
-        position: fixed;
-        border-radius: 5px;
+            border-radius: 5px;
+            border: solid var(--borderColor) 1.5px;
+            
+            
+            padding-bottom: 5px; padding-top: 5px;
+            padding-left: 5rem; padding-right: 5rem;
 
-        margin: auto;
+            border-width: 1.5px;
+            border: none;
+            border-color: var(--borderColor);
+            
 
-        padding: 15px;
+            transition: 0.1s;
+            
+        }
 
-        border: solid 2px;
+        .cameraContainer {
+            display: flex;
+        }
 
-        border-color: var(--borderColor);
-        box-shadow: 0 0 5px 2.5px var(--borderGlow);
+        .camImage {
+            /* position: fixed; */
+            margin-left: auto;
+            margin-right: auto;
+        }
 
-        justify-content: center;
-        display: flex;
         
-
-    }
-
-    select {
-        color: var(--borderColor);
-        background-color: rgb(11, 1, 28);
-        margin-left: auto; margin-right: auto;
-
-        border-radius: 5px;
-        border: solid var(--borderColor) 1.5px;
-        
-        
-        padding-bottom: 5px; padding-top: 5px;
-        padding-left: 5rem; padding-right: 5rem;
-
-        border-width: 1.5px;
-        border: none;
-        border-color: var(--borderColor);
-        
-
-        transition: 0.1s;
-        
-    }
-
-    img {
-        border-radius: 5px;
-
-    }
-
-    
-</style>
+    </style>
